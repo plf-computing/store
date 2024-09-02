ea02ae3e70d09e15428cd2925a2675e29d5b63a1
plf-computing <plfcomputing@gmail.com>
2024-09-02 15:26:52 +0300

    updates on cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { StoreApiService } from '../../services/store-api.service';
+import { Product } from '../../model/product';
+import { DecimalPipe, NgFor, NgIf } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [NgIf,NgFor, RouterLink,RouterLinkActive,DecimalPipe],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  cartItems:Product[]=[];
+  totalAmount:number=0;
+
+  constructor(private storeApi:StoreApiService){}
+
+  ngOnInit(){
+    this.cartItems = this.storeApi.getCartItems();
+    this.calcTotalAmount( );
+  }
+  calcTotalAmount(){
+    this.totalAmount = this.cartItems.reduce((total,item)=> total  + (item.price * item.quantity),0)
+  }
+
+  removeProduct(product:Product):void{
+    this.storeApi.removeCartItem(product)
+    this.calcTotalAmount()
+
+  }
 
 }
--- src/app/services/store-api.service.ts
+++ src/app/services/store-api.service.ts
@@ -9,11 +9,18 @@ import { NotificationsService } from './notifications.service';
 })
 export class StoreApiService {
 
-  private cartItems:Product[]=[]
+  private cartItems:Product[]=[];
+  private storageKey = 'cartItems'
 
 
 
-  constructor(private http: HttpClient,private notService:NotificationsService) { }
+  constructor(private http: HttpClient,private notService:NotificationsService) { 
+    const storedItems = localStorage.getItem(this.storageKey)
+    if(storedItems){
+      this.cartItems = JSON.parse(storedItems)
+
+    }
+  }
 
 getProducts():Observable<Product[]>{
   return this.http.get<Product[]>('https://fakestoreapi.com/products')
@@ -30,12 +37,27 @@ getProducts():Observable<Product[]>{
   this.cartItems.push(product);
   this.notService.showSuccess(`${product.title} added in the cart`)
  }
+ localStorage.setItem(this.storageKey,JSON.stringify(this.cartItems))
  }
+
+
 
  getCartItemCount():number{
   return this.cartItems.length
  }
  getCartItems():Product[]{
   return this.cartItems;
+ }
+
+ removeCartItem(product:Product):void{
+  const index = this.cartItems.findIndex(item =>item.id === product.id)
+  if(index != -1){
+    this.cartItems.splice(index,1);
+    localStorage.setItem(this.storageKey,JSON.stringify(this.cartItems))
+    this.notService.showSuccess('Product removed from the cart')
+
+
+  }
+
  }
 }
